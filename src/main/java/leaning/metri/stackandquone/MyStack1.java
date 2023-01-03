e8b385df9f30317ba9bf94d5765eb23dd847c624
wuerzhen <1160115913@qq.com>
2023-01-03 22:29:38 +0800

    pop

---
+++ src/main/java/leaning/metri/stackandquone/MyStack1.java
@@ -0,0 +1,13 @@
+package leaning.metri.stackandquone;
+
+import java.util.Stack;
+
+public class MyStack1 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack1() {
+        this.stackData = new Stack<Integer>();
+        this.stackMin = new Stack<Integer>();
+    }
+}
