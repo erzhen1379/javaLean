4b8e93e73de53477e1ad32c5ce89baafcdbc5471
wuerzhen <1160115913@qq.com>
2023-07-03 21:10:10 +0800

    HJ20,21

--- src/main/java/leaning/structure/leetCode/huawei/HJ20.java
+++ src/main/java/leaning/structure/leetCode/huawei/HJ20.java
@@ -1,6 +1,8 @@
 package leaning.structure.leetCode.huawei;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -14,28 +16,53 @@ public class HJ20 {
         if (str.length() < 8) {
             System.out.println("NG");
         } else {
+            boolean flag = true;
             int count = 0;
             Pattern p1 = Pattern.compile("[A-Z]");
             Pattern p2 = Pattern.compile("[a-z]");
             Pattern p3 = Pattern.compile("[0-9]");
             Pattern p4 = Pattern.compile("[^a-zA-Z0-9]");
+            boolean isA = false;
+            boolean isa = false;
+            boolean isNum = false;
+            boolean isOther = false;
             for (int i = 0; i < chars.length; i++) {
                 if (p1.matcher(String.valueOf(chars[i])).find()) {
-                    count++;
+                    if (isA == false) {
+                        count++;
+                    }
+                    isA = true;
                 }
                 if (p2.matcher(String.valueOf(chars[i])).find()) {
-                    count++;
+                    if (isa == false) {
+                        count++;
+                    }
+                    isa = true;
                 }
                 if (p3.matcher(String.valueOf(chars[i])).find()) {
-                    count++;
+                    if (isNum == false) {
+                        count++;
+                    }
+                    isNum = true;
                 }
                 if (p4.matcher(String.valueOf(chars[i])).find()) {
-                    count++;
+                    if (isOther == false) {
+                        count++;
+                    }
+                    isOther = true;
+                }
+                //判断长度是否
+                if (i + 1 < chars.length) {
+                    String tmp = chars[i] + "" + chars[i + 1];
+                    if (str.replace(tmp, "").length() != (str.length() - 2)) {
+                        System.out.println(tmp);
+                        flag = false;
+                    }
                 }
             }
-            if (count>2){
+            if (count >= 3 && flag) {
                 System.out.println("OK");
-            }else {
+            } else {
                 System.out.println("NG");
             }
         }
---
+++ src/main/java/leaning/structure/leetCode/huawei/HJ22.java
@@ -0,0 +1,12 @@
+package leaning.structure.leetCode.huawei;
+
+/**
+ * 某商店规定：三个空汽水瓶可以换一瓶汽水，允许向老板借空汽水瓶（但是必须要归还）。
+ * 小张手上有n个空汽水瓶，她想知道自己最多可以喝到多少瓶汽水。
+ * 数据范围：输入的正整数满足 1≤n≤100
+ */
+public class HJ22 {
+    public static void main(String[] args) {
+
+    }
+}
