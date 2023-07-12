0dd45f25b011b5ceb03649d9e084f03f4e8ea8b0
wuerzhen <1160115913@qq.com>
2023-07-12 14:51:55 +0800

    HJ20,21

--- src/main/java/leaning/structure/leetCode/huawei/HJ22.java
+++ src/main/java/leaning/structure/leetCode/huawei/HJ22.java
@@ -1,5 +1,7 @@
 package leaning.structure.leetCode.huawei;
 
+import java.util.Scanner;
+
 /**
  * 某商店规定：三个空汽水瓶可以换一瓶汽水，允许向老板借空汽水瓶（但是必须要归还）。
  * 小张手上有n个空汽水瓶，她想知道自己最多可以喝到多少瓶汽水。
@@ -7,6 +9,21 @@ package leaning.structure.leetCode.huawei;
  */
 public class HJ22 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while (true) {
+            int num = scanner.nextInt();
+            count++;
+            if (count == 10) {
+                break;
+            }
+            //注意判断字符串是否相等
+            if (num == 0) {
+                break;
+            }
+            System.out.println(num / 3);
+            int empty = num % 3;
 
+        }
     }
 }
